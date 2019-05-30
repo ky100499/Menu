aa9f2637cf56f63e8414d0a2dea554332606a867
Kwangyeon Lee <ky100499@hanyang.ac.kr>
2019-05-31 03:28:44 +0900

    Foods Template

--- routes/main/index.js
+++ routes/main/index.js
@@ -4,7 +4,19 @@ const router = require('express').Router()
 
 router
 .get('/', (req, res) => {
-    res.render('main')
+    res.redirect('/main')
+})
+.get('/main', (req, res) => {
+    res.render('main/main')
+})
+.get('/side', (req, res) => {
+    res.render('main/side')
+})
+.get('/fruit', (req, res) => {
+    res.render('main/fruit')
+})
+.get('/menu', (req, res) => {
+    res.render('main/menu')
 })
 
 module.exports = router
